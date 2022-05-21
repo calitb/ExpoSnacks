be200065ffa320269e54f0b64601d2e038d63ab4
Carlos Thurber <thurber.carlos@gmail.com>
2022-05-21 17:24:21 -0500

    unmatched---cards: Adds prompt with additional info in Details

--- unmatched---cards/App.js
+++ unmatched---cards/App.js
@@ -3,23 +3,30 @@ import Home from "./screens/Home.jsx"
 import Details from "./screens/Details.jsx"
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-native-paper';
+import { FontAwesome } from '@expo/vector-icons';
 
 const MainStack = createNativeStackNavigator();
 export default function App() {
   return (
-    <NavigationContainer>
-      <MainStack.Navigator>
-        <MainStack.Screen
-          name='Home'
-          component={Home}
-          options={{title: 'Unmatched'}}
-        />
-        <MainStack.Screen
-          name='Details'
-          component={Details}
-          options={{headerBackTitle:'Volver'}}
-        />
-      </MainStack.Navigator>
-    </NavigationContainer>
+    <Provider
+      settings={{
+        icon: (props) => <FontAwesome {...props} />,
+      }}>
+      <NavigationContainer>
+        <MainStack.Navigator>
+          <MainStack.Screen
+            name='Home'
+            component={Home}
+            options={{ title: 'Unmatched' }}
+          />
+          <MainStack.Screen
+            name='Details'
+            component={Details}
+            options={{ headerBackTitle: 'Volver' }}
+          />
+        </MainStack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
--- unmatched---cards/screens/Details.jsx
+++ unmatched---cards/screens/Details.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { Button, IconButton, Paragraph, Dialog, Portal, Provider } from 'react-native-paper';
 
 const BOOST = "⚫️";
 const types = {
@@ -12,43 +13,65 @@ const types = {
 export default function Details(props) {
   const character = props.route.params.character;
 
+  const [visible, setVisible] = React.useState(false);
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
+
   React.useLayoutEffect(() => {
     props.navigation.setOptions({
       title: `${character.name} (${character.amount ?? ''})`,
+      headerRight: () => <IconButton icon="info" size={24} onPress={showDialog} color="#000" />,
     });
   }, [props.navigation, props.route.params.character]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView style={styles.container}>
-        {character.cards.map((card) => {
-          const { name, descripcion, type, value, amount, character, boost } = card;
-          const typeIcon = types[type];
+    <Provider>
+      <SafeAreaView style={styles.container}>
+        <Portal>
+          <Dialog visible={visible} onDismiss={hideDialog}>
+            <Dialog.Title>Unmatched Attacks</Dialog.Title>
+            <Dialog.Content>
+              <Paragraph>{`Orden de Efectos\n1. Immediately (Defender)\n2. Immediately (Attacker)\n3. During Combat (Defender)\n4. During Combat (Attacker)\n5. After Combar (Defender)\n6. After Combar (Attacker)`}</Paragraph>
+            </Dialog.Content>
+            <Dialog.Content>
+              <Paragraph>El combate lo gana el atacante si logra hacer daño, en caso contrario lo gana el defensor.</Paragraph>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button onPress={hideDialog}>OK</Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
 
-          return (
-            <View style={styles.itemContainer}>
-              <Text style={styles.title}>
-                {name}
-                {character !== 'any' &&
-                  <Text style={styles.title}>
-                    {" "}({character})
-                  </Text>}
-                {"\n"}
-              </Text>
-              <View style={styles.contentView}>
-                <Text style={styles.subtitle}>
-                  {amount > 1 ? `${amount}x ` : ''}
-                  {typeIcon.padStart(value * typeIcon.length, typeIcon)}
-                  {BOOST.padStart(boost * BOOST.length, BOOST)}
+        <ScrollView style={styles.container}>
+          {character.cards.map((card) => {
+            const { name, descripcion, type, value, amount, character, boost } = card;
+            const typeIcon = types[type];
+
+            return (
+              <View style={styles.itemContainer}>
+                <Text style={styles.title}>
+                  {name}
+                  {character !== 'any' &&
+                    <Text style={styles.title}>
+                      {" "}({character})
+                    </Text>}
+                  {"\n"}
                 </Text>
-                <Text style={styles.description}>{descripcion}</Text>
+                <View style={styles.contentView}>
+                  <Text style={styles.subtitle}>
+                    {amount > 1 ? `${amount}x ` : ''}
+                    {typeIcon.padStart(value * typeIcon.length, typeIcon)}
+                    {BOOST.padStart(boost * BOOST.length, BOOST)}
+                  </Text>
+                  <Text style={styles.description}>{descripcion}</Text>
+                </View>
               </View>
-            </View>
-          )
-        }
-        )}
-      </ScrollView>
-    </SafeAreaView>
+            )
+          }
+          )}
+        </ScrollView>
+      </SafeAreaView>
+    </Provider>
   );
 }
 
